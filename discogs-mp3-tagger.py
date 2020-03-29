7c79104a50875ae796df370600cf7307d1b8ac13
Thomas JOUANNOT <mazerty@gmail.com>
2020-03-29 17:40:58 +0200

    added some logs

--- discogs-mp3-tagger.py
+++ discogs-mp3-tagger.py
@@ -11,6 +11,7 @@ def refresh():
     if data_file.exists():
         data = yaml.load(data_file.read_text(encoding="utf-8"), Loader=yaml.BaseLoader) or []
     else:
+        print("data file {!s} not found, will be created".format(data_file))
         data = []
 
     directories_in_source = [x.name for x in source.iterdir() if x.is_dir()]
@@ -20,12 +21,17 @@ def refresh():
     if missing_directories:
         raise Exception("missing directories in data", missing_directories)
 
-    [data.append({"name": x,
-                  "release_id": "todo",
-                  "selected": [],
-                  "source": "todo",
-                  "tracks": [{"name": x.name, "position": 0} for x in sorted(source.joinpath(x).iterdir())]})
-     for x in directories_in_source if x not in directories_in_data]
+    missing_data = [x for x in directories_in_source if x not in directories_in_data]
+    if missing_data:
+        for x in missing_data:
+            data.append({"name": x,
+                         "release_id": "todo",
+                         "selected": [],
+                         "source": "todo",
+                         "tracks": [{"name": x.name, "position": 0} for x in sorted(source.joinpath(x).iterdir())]})
+        print("added {} directories, don't forget to customize them before proceeding".format(len(missing_data)))
+    else:
+        print("nothing added")
 
     data_file.write_text(yaml.dump(sorted(data, key=lambda x: x.get("name").lower())), encoding="utf-8")
 
