6a0b1f82f27ea8d824d604ae376ba3139a6d19e2
Thomas JOUANNOT <mazerty@gmail.com>
2020-04-01 17:33:37 +0200

    plan

--- discogs-mp3-tagger.py
+++ discogs-mp3-tagger.py
@@ -1,6 +1,8 @@
 import argparse
 import pathlib
+import pprint
 
+import requests
 import yaml
 
 source = pathlib.Path("/mnt/perm/musique/source")
@@ -22,7 +24,22 @@ def prepare():
     prepare_file.write_text(yaml.dump(content), encoding="utf-8")
 
 
+def plan():
+    prepare_data = yaml.load(prepare_file.read_text(encoding="utf-8"), Loader=yaml.BaseLoader)
+
+    if filter(lambda x: x.get("release_id") == "todo", prepare_data):
+        raise Exception("data needs to be customized")
+
+    for y in prepare_data:
+        json = session.get("https://api.discogs.com/releases/" + y.get("release")).json()
+        pprint.pprint(json)
+
+
+session = requests.Session()
+
 argparser = argparse.ArgumentParser(description="")
-argparser.add_argument("command", choices=["prepare"])
+argparser.add_argument("command", choices=["prepare", "plan"])
 args = argparser.parse_args()
 globals()[args.command]()
+
+session.close()
