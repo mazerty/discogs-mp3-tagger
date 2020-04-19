65babf77b4c2f9ee241e54cab46df7b843ea16b1
Thomas JOUANNOT <mazerty@gmail.com>
2020-04-19 14:24:51 +0200

    write mp3 and tag

--- discogs-mp3-tagger.py
+++ discogs-mp3-tagger.py
@@ -1,9 +1,11 @@
 import argparse
 import json
 import pathlib
+import shutil
 import sys
 import time
 
+import eyed3
 import requests
 import yaml
 
@@ -11,6 +13,7 @@ source = pathlib.Path("/mnt/perm/musique/source")
 prepare_file = pathlib.Path("/home/ubuntu/workspace/discogs-mp3-tagger-data/prepare.yaml")
 cache = pathlib.Path("/home/ubuntu/workspace/discogs-mp3-tagger-data/cache")
 plan_file = pathlib.Path("/home/ubuntu/workspace/discogs-mp3-tagger-data/plan.yaml")
+target = pathlib.Path("/mnt/perm/musique/target")
 
 
 def prepare():
@@ -75,10 +78,29 @@ def plan():
     plan_file.write_text(yaml.dump(content, width=sys.maxsize), encoding="utf-8")
 
 
+def apply():
+    plan_data = yaml.load(plan_file.read_text(encoding="utf-8"), Loader=yaml.BaseLoader)
+    for item in plan_data:
+        album = target.joinpath(item.get("artist") + " - " + item.get("album") + " - " + item.get("release_id"))
+        if not album.is_dir():
+            album.mkdir()
+            for track in item.get("tracks"):
+                file = album.joinpath(track.get("position") + " - " + track.get("title") + ".mp3")
+                shutil.copy(source.joinpath(item.get("name")).joinpath(track.get("name")), file)
+
+                mp3 = eyed3.load(file)
+                mp3.initTag()
+                mp3.tag.artist = item.get("artist")
+                mp3.tag.album = item.get("album")
+                mp3.tag.track_num = int(track.get("position"))
+                mp3.tag.title = track.get("title")
+                mp3.tag.save()
+
+
 session = requests.Session()
 
 argparser = argparse.ArgumentParser(description="")
-argparser.add_argument("command", choices=["prepare", "plan"])
+argparser.add_argument("command", choices=["prepare", "plan", "apply"])
 args = argparser.parse_args()
 globals()[args.command]()
 
